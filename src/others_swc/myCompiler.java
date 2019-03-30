6b78a5dfa0e213b6851fe9c884f3fcfef8dbba35
palashcse2k8 <palashcse2k8@gmail.com>
2019-03-30 13:14:22 +0600

    mycompiler fix for multiple input

--- src/others_swc/myCompiler.java
+++ src/others_swc/myCompiler.java
@@ -1,4 +1,25 @@
 package others_swc;
+
+/*
+try these input also works for lower case operation name
+ADD 2 2 2
+MULT 2 2 2
+add 2 2 2
+mult 2 2 2
+ADD X, 4
+SQRT 9
+HELLO CLASS
+ASSIGN X, 10
+ADD X, 10
+PRINT X
+PRINT ASSIGN Y, X
+ADD X, Y
+PRINT ADD POW X, 2, 4
+SUB Y, $
+PRINT ::))
+PRINT 100
+ADD PRINT ASSIGN Z, 100, 4
+ */
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
@@ -12,46 +33,64 @@ import java.util.Stack;
 
 
 public class myCompiler {
-	
-	static Map<String, Integer> myVariables = new HashMap<String, Integer>();
-	
-	Stack<String> myOperations = new Stack<>();
-	Stack<String> Variables = new Stack<>();
-	Deque<String> myValues = new ArrayDeque<>();
 
-	static String [] Operations = {"ADD", "SUB", "MULT", "DIV", "POW", "ASSIGN", "PRINT"};
+	static Map<String, Integer> myVariables = new HashMap<String, Integer>();
+
+	Stack<String> myOperations = new Stack<>(); //to add the valid operations
+	Stack<String> Variables = new Stack<>();
+	Deque<String> myValues = new ArrayDeque<>(); // to add the valid values and variables, used dequeue to add/remove in both side
+
+	static String [] Operations = {"ADD", "SUB", "MULT", "DIV", "POW", "ASSIGN", "PRINT"}; //set of valid operations
 	static Set<String> operationSet = new HashSet<String>(Arrays.asList(Operations)); 
-	
-	static int flag = 0;
-	
+
+	static int flag = 0; //to terminate for unexpected command
+
+	//checking the input validity only digit or alphabet
 	public boolean isValidInput(String str)
 	{
 		return ((!str.equals("")) && (str != null) && (str.matches("^[a-zA-Z]*$")) || (str.matches("^[0-9]*$")));
 	}
-	
+
+
+	//doing operations
 	public void doOperation(String operation)
 	{
 		String argument1 = null, argument2;
-		
+
 		if(operation.equals("ASSIGN") == true)
 		{
 			argument1 = myValues.remove();
 			argument2 = myValues.remove();
-			
+
+			//checking the 2nd input is variable or digit			
 			if(isStringOnlyAlphabet(argument2))
 			{
+				// if variable checking if it is in the dictionary already
 				if(!myVariables.containsKey(argument2))
 				{
 					System.out.println("UNEXPECTED COMMAND");
 					flag = 1;
 					return;
 				}
+				// convert integer to string
 				argument2 = myVariables.get(argument2).toString();
 			}
-			
-			myVariables.put(argument1, Integer.parseInt(argument2));
-			myValues.addFirst(argument1);
-			System.out.println("SUCCESS");
+
+			if(isStringOnlyAlphabet(argument1))
+			{
+				myVariables.put(argument1, Integer.parseInt(argument2));
+				// adding if x 100 x = 100
+				myValues.addFirst(argument1);
+				System.out.println("SUCCESS");
+			}
+			else
+			{
+				System.out.println("UNEXPECTED COMMAND");
+				flag = 1;
+				return;
+			}
+
+
 		}
 		else if(operation.equals("PRINT") == true)
 		{
@@ -72,16 +111,14 @@ public class myCompiler {
 			else
 			{
 				System.out.println("PRINTING " + argument1);
-			}
-
-			
+			}			
 		}
 		else if(operation.equals("ADD") == true)
 		{
-			
+
 			argument1 = myValues.remove();
 			argument2 = myValues.remove();
-			
+
 			if(isStringOnlyAlphabet(argument2))
 			{
 				if(!myVariables.containsKey(argument2))
@@ -92,12 +129,20 @@ public class myCompiler {
 				}
 				argument2 = myVariables.get(argument2).toString();
 			}
-			
+
 			if(isStringOnlyAlphabet(argument1))
 			{
 				if(myVariables.containsKey(argument1))
 				{
 					myVariables.put(argument1, myVariables.get(argument1) + Integer.parseInt(argument2));
+
+					if(myOperations.isEmpty() && !myValues.isEmpty())
+					{
+						while(!myValues.isEmpty())
+						{
+							myVariables.put(argument1, myVariables.get(argument1) + Integer.parseInt(myValues.remove()));
+						}		
+					}					
 					myValues.addFirst(argument1);
 					System.out.println("SUCCESS");
 				}
@@ -107,19 +152,25 @@ public class myCompiler {
 					return;
 				}
 			}
-			else
+			else // if all the values are integer and there is varibale
 			{
-				System.out.println(argument2 + argument1);
+				int result = Integer.parseInt(argument1) + Integer.parseInt(argument2);
+				while(!myValues.isEmpty())
+				{
+					result += Integer.parseInt(myValues.remove());
+				}
+
+				System.out.println(result);
 				System.out.println("SUCCEESS");
 			}
 
 		}
 		else if(operation.equals("POW") == true)
 		{
-			
+
 			argument1 = myValues.remove();
 			argument2 = myValues.remove();
-			
+
 			if(isStringOnlyAlphabet(argument2))
 			{
 				if(!myVariables.containsKey(argument2))
@@ -130,7 +181,7 @@ public class myCompiler {
 				}
 				argument2 = myVariables.get(argument2).toString();
 			}
-			
+
 			if(isStringOnlyAlphabet(argument1))
 			{
 				if(myVariables.containsKey(argument1))
@@ -141,7 +192,7 @@ public class myCompiler {
 				}
 				else
 				{
-					System.out.println("NO VALUE ASSGIN TO " + argument1);
+					System.out.println("No value found for  " + argument1);
 				}
 			}
 			else
@@ -153,10 +204,10 @@ public class myCompiler {
 		}
 		else if(operation.equals("SUB") == true)
 		{
-			
+
 			argument1 = myValues.remove();
 			argument2 = myValues.remove();
-			
+
 			if(isStringOnlyAlphabet(argument2) )
 			{
 				if(!myVariables.containsKey(argument2))
@@ -167,7 +218,7 @@ public class myCompiler {
 				}
 				argument2 = myVariables.get(argument2).toString();
 			}
-			
+
 			if(isStringOnlyAlphabet(argument1))
 			{
 				if(myVariables.containsKey(argument1))
@@ -192,28 +243,35 @@ public class myCompiler {
 		}
 		else if(operation.equals("MULT") == true)
 		{
-			
+
 			argument1 = myValues.remove();
 			argument2 = myValues.remove();
-			
+
 			if(isStringOnlyAlphabet(argument2))
 			{
-				
+
 				if(!myVariables.containsKey(argument2))
 				{
 					System.out.println("UNEXPECTED COMMAND");
 					flag = 1;
 					return;
 				}
-				
+
 				argument2 = myVariables.get(argument2).toString();
 			}
-			
+
 			if(isStringOnlyAlphabet(argument1))
 			{
 				if(myVariables.containsKey(argument1))
 				{
 					myVariables.put(argument1, myVariables.get(argument1) * Integer.parseInt(argument2));
+					if(myOperations.isEmpty() && !myValues.isEmpty())
+					{
+						while(!myValues.isEmpty())
+						{
+							myVariables.put(argument1, myVariables.get(argument1) * Integer.parseInt(myValues.remove()));
+						}		
+					}	
 					myValues.addFirst(argument1);
 					System.out.println("SUCCESS");
 				}
@@ -222,19 +280,25 @@ public class myCompiler {
 					System.out.println("NO VALUE ASSGIN TO " + argument1);
 				}
 			}
-			else
+			else // if all the inputs are digit and no variable to parse
 			{
-				System.out.println(argument2 + argument1);
+				int result = Integer.parseInt(argument1) * Integer.parseInt(argument2);
+				while(!myValues.isEmpty())
+				{
+					result *= Integer.parseInt(myValues.remove());
+				}
+
+				System.out.println(result);
 				System.out.println("SUCCEESS");
 			}
 
 		}
 		else if(operation.equals("DIV") == true)
 		{
-			
+
 			argument1 = myValues.remove();
 			argument2 = myValues.remove();
-			
+
 			if(isStringOnlyAlphabet(argument2))
 			{
 				if(!myVariables.containsKey(argument2))
@@ -245,7 +309,7 @@ public class myCompiler {
 				}
 				argument2 = myVariables.get(argument2).toString();
 			}
-			
+
 			if(isStringOnlyAlphabet(argument1))
 			{
 				if(myVariables.containsKey(argument1))
@@ -261,7 +325,7 @@ public class myCompiler {
 			}
 			else
 			{
-				System.out.println(argument2 + argument1);
+				System.out.println(Integer.parseInt(argument1) / Integer.parseInt(argument2));
 				System.out.println("SUCCEESS");
 			}
 
@@ -271,23 +335,27 @@ public class myCompiler {
 			//System.out.println(myValues.pop() - myValues.pop());
 			System.out.println("NOTHING MATCHED");
 		}
-		
+
 
 	}
-	
+
+	//checking only alphabet
 	public static boolean isStringOnlyAlphabet(String str) 
 	{ 
-	    return ((!str.equals("")) && (str != null) && (str.matches("^[a-zA-Z]*$")));
+		return ((!str.equals("")) && (str != null) && (str.matches("^[a-zA-Z]*$")));
 	}
-	
+
+	//checking only digits 
 	public static boolean isStringOnlyDigits(String str) 
 	{ 
-	    return ((!str.equals("")) && (str != null) && (str.matches("^[0-9]*$")));
+		return ((!str.equals("")) && (str != null) && (str.matches("^[0-9]*$")));
 	} 
-	
-	
+
+
+	//parsing the tokens
 	public void parseToken()
 	{
+		//checking the stack is empty or not, will continue until the stack is empty
 		while(!myOperations.isEmpty())
 		{
 			String operation = myOperations.pop();
@@ -298,31 +366,35 @@ public class myCompiler {
 			}
 		}
 	}
-	
-	
+
+
 	public void makeToken(String str)
 	{
-		
+
+		//spliting the input against space and comma
 		String [] tokens = str.split(" |,");
-		//System.out.println(tokens.length);
-		
+
 		for (int i = 0; i < tokens.length; i++) {
-			
-			
+
+			tokens[i] = tokens[i].toUpperCase();
+
+			//checking the first token validity
 			if(i==0 && operationSet.contains(tokens[i]) == false)
 			{
 				System.out.println("UNEXPECTED COMMAND");
 				flag = 1;
 				return;
 			}
-			
+
+			//add the valid operation in stack
 			if(operationSet.contains(tokens[i]) == true)
 			{
 
 				myOperations.push(tokens[i]);
 			}
 			else{
-				
+
+				//adding valid input in de queue
 				if(tokens[i] != null && !tokens[i].equals(""))
 				{
 					if(isValidInput(tokens[i]))
@@ -335,35 +407,40 @@ public class myCompiler {
 						flag = 1;
 						return;
 					}
-					
+
 				}
-			
+
 			}
 		}
 	}
-	
-	
-	
-	public static void main(String[] args) {
-		
+
+
+
+	public static void main(String[] args) 
+	{
 		Scanner myScanner = new Scanner(System.in);
-		
+
 		String inputLine ;
 		myCompiler compiler = new myCompiler();
-		
+
+		//to get continuous input 
 		while (true)
 		{	
-			
+			//taking string input as line
 			inputLine = myScanner.nextLine();
+
+			//intializing the data structure
 			flag = 0;
 			compiler.myOperations = new Stack<>();
 			compiler.Variables = new Stack<>();
 			compiler.myValues = new ArrayDeque<>();
 
+			// tokenize the input
 			compiler.makeToken(inputLine);
-			
+
 			if(flag == 0)
 			{
+				//parsing the input
 				compiler.parseToken();
 			}
 		}
